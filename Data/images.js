ae5c56a39126e0a8537cd72fec7937d6c341a696
marionc22 <103053810+marionc22@users.noreply.github.com>
2022-06-22 16:47:06 +0200

    Update images.js

--- Data/images.js
+++ Data/images.js
@@ -1,59 +1,59 @@
 const  images = [
    {
-    pic : "https://media.vogue.fr/photos/5c2f4cca66249b0f58f396b9/2:3/w_1920,c_limit/mood_gosling_8365.jpeg",
+    pic :"https://p3.storage.canalblog.com/35/24/118811/43230943_p.jpg"
+    correct_answer: "francky vincent"
+   },
+   {
+    pic :  "https://media.vogue.fr/photos/5c2f4cca66249b0f58f396b9/2:3/w_1920,c_limit/mood_gosling_8365.jpeg",
     correct_answer:"ryan gosling"
    },
    {
-    pic : "",
-    correct_answer:""
+    pic : "https://i0.wp.com/www.starmag.com/wp-content/uploads/2018/08/miakhalifa.jpg?resize=1200%2C1200&ssl=1",
+    correct_answer:"mia khalifa"
    },
    {
-    pic : "",
-    correct_answer:""
+    pic : "https://media.vidoleo.com/img/profil/tonyvairelles.jpg",
+    correct_answer:"tony vairelles"
    },
    {
-    pic : "",
-    correct_answer:""
+    pic : "https://i.pinimg.com/originals/40/d5/2a/40d52ad44a3ce305a1dae2c22fb46222.jpg",
+    correct_answer:"lebron james"
    },
    {
-    pic : "",
-    correct_answer:""
+    pic : "https://upload.wikimedia.org/wikipedia/commons/e/e2/Simone_Veil_%281984%29.jpg",
+    correct_answer:"simone veil"
    },
    {
-    pic : "",
-    correct_answer:""
+    pic : "https://fr.web.img6.acsta.net/c_310_420/medias/nmedia/18/35/44/90/20413395.jpg",
+    correct_answer:"audrey hepburn"
    },
    {
-    pic : "",
-    correct_answer:""
+    pic : "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Rocco_Siffredi_2010.jpg/400px-Rocco_Siffredi_2010.jpg",
+    correct_answer:"rocco siffredi"
    },
    {
-    pic : "",
-    correct_answer:""
+    pic : "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Kanye_West_at_the_2009_Tribeca_Film_Festival-2_%28cropped%29.jpg/800px-Kanye_West_at_the_2009_Tribeca_Film_Festival-2_%28cropped%29.jpg",
+    correct_answer:"kanye west"
    },
    {
-    pic : "",
-    correct_answer:""
+    pic : "https://creapills.com/wp-content/uploads/2021/03/bob-leponge-humains-8.png",
+    correct_answer:"carlo"
    },
    {
-    pic : "",
-    correct_answer:""
+    pic : "https://anniversaire-celebrite.com/upload/250x333/bob-razowski-250.jpg",
+    correct_answer:"bob"
    },
    {
-    pic : "",
-    correct_answer:""
+    pic : "https://www.pokepedia.fr/images/thumb/c/cc/Carapuce-RFVF.png/250px-Carapuce-RFVF.png",
+    correct_answer:"carapuce"
    },
    {
-    pic : "",
-    correct_answer:""
+    pic : "https://upload.wikimedia.org/wikipedia/commons/4/4d/IggyChesterRocks.jpg",
+    correct_answer:"iggy pop"
    },
    {
-    pic : "",
-    correct_answer:""
-   },
-   {
-    pic : "",
-    correct_answer:""
+    pic : "https://www.larousse.fr/encyclopedie/data/images/1315441-Staline.jpg",
+    correct_answer:"staline"
    },
    {
     pic : "",
