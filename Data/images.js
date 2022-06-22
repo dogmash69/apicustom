cdddbf30cf2adf78b8a7cca436dd7173b99f3451
dogmash69 <103027893+dogmash69@users.noreply.github.com>
2022-06-22 15:21:03 +0200

    maj 22/06

---
+++ Data/images.js
@@ -0,0 +1,83 @@
+const  images = [
+   {
+    pic : "https://media.vogue.fr/photos/5c2f4cca66249b0f58f396b9/2:3/w_1920,c_limit/mood_gosling_8365.jpeg",
+    correct_answer:"ryan gosling"
+   },
+   {
+    pic : "",
+    correct_answer:""
+   },
+   {
+    pic : "",
+    correct_answer:""
+   },
+   {
+    pic : "",
+    correct_answer:""
+   },
+   {
+    pic : "",
+    correct_answer:""
+   },
+   {
+    pic : "",
+    correct_answer:""
+   },
+   {
+    pic : "",
+    correct_answer:""
+   },
+   {
+    pic : "",
+    correct_answer:""
+   },
+   {
+    pic : "",
+    correct_answer:""
+   },
+   {
+    pic : "",
+    correct_answer:""
+   },
+   {
+    pic : "",
+    correct_answer:""
+   },
+   {
+    pic : "",
+    correct_answer:""
+   },
+   {
+    pic : "",
+    correct_answer:""
+   },
+   {
+    pic : "",
+    correct_answer:""
+   },
+   {
+    pic : "",
+    correct_answer:""
+   },
+   {
+    pic : "",
+    correct_answer:""
+   },
+   {
+    pic : "",
+    correct_answer:""
+   },
+   {
+    pic : "",
+    correct_answer:""
+   },
+   {
+    pic : "",
+    correct_answer:""
+   },
+   {
+    pic : "",
+    correct_answer:""
+   },
+]
+export default images
